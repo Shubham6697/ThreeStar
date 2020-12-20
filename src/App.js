98873c56812b116b62cdfb29eab92c1c362754bd
ekmavivek <vivekkumar0252525@gmail.com>
2020-12-20 17:56:59 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Student from "./student";
+
+const App = ()=> {
+    return (
+        <div>
+            <Student name="Vivek" />
+            <Student name="Sumit" />
+            <Student name="panku" />
+
+        </div>
+    );
+};
+
+export default App;
